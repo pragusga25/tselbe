2b6400c1df8ac2322d028d19604e0c4d3825b091
Taufik Pragusga <taufik@pragusga.com>
2024-08-23 16:02:58 +0700

    change principalUserId schema

--- src/user/validations.ts
+++ src/user/validations.ts
@@ -1,113 +1,104 @@
 import {
-	type Output,
-	array,
-	email,
-	minLength,
-	object,
-	optional,
-	string,
-	uuid,
-} from "valibot";
-import { PrincipalTypeSchema } from "../aws-identity/validations";
+  type Output,
+  array,
+  email,
+  minLength,
+  object,
+  optional,
+  string,
+} from 'valibot';
+import { PrincipalTypeSchema } from '../aws-identity/validations';
 
 export const DeleteAccountsSchema = object({
-	ids: array(
-		string("Id must be a string", [minLength(1)]),
-		"The input must be an array of ids.",
-		[minLength(1, "Please input at least one id.")],
-	),
+  ids: array(
+    string('Id must be a string', [minLength(1)]),
+    'The input must be an array of ids.',
+    [minLength(1, 'Please input at least one id.')]
+  ),
 });
 
 export const UpdateAccountPasswordSchema = object({
-	// userId: string('User id must be a string', [minLength(1)]),
-	oldPassword: string("Password must be a string", [minLength(1)]),
-	newPassword: string("New password must be a string", [minLength(8)]),
+  // userId: string('User id must be a string', [minLength(1)]),
+  oldPassword: string('Password must be a string', [minLength(1)]),
+  newPassword: string('New password must be a string', [minLength(8)]),
 });
 
 export const ResetAccountUserPasswordSchema = object({
-	userId: string("User id must be a string", [minLength(1)]),
+  userId: string('User id must be a string', [minLength(1)]),
 });
 
 export const PrincipalAwsAccountUser = object({
-	principalId: string("Principal id must be a string", [
-		uuid("Please enter a valid principal id."),
-	]),
-	principalType: PrincipalTypeSchema,
-	awsAccountId: string("AWS account id must be a string", [
-		minLength(1, "Please enter the AWS account id."),
-	]),
+  principalId: string('Principal id must be a string'),
+  principalType: PrincipalTypeSchema,
+  awsAccountId: string('AWS account id must be a string', [
+    minLength(1, 'Please enter the AWS account id.'),
+  ]),
 });
 
 export const CreateAccountUserSchema = object({
-	username: string("Username must be a string", [
-		minLength(1, "Please enter the username."),
-	]),
-	password: string("Password must be a string", [
-		minLength(1, "Please enter the password."),
-	]),
-	name: string("Name must be a string", [
-		minLength(1, "Please enter the name."),
-	]),
-	principalUserId: string("Principal user id must be a string", [
-		uuid("Please enter a valid principal user id."),
-	]),
-	email: string("Email must be a string", [
-		email("Please enter a valid email address."),
-	]),
+  username: string('Username must be a string', [
+    minLength(1, 'Please enter the username.'),
+  ]),
+  password: string('Password must be a string', [
+    minLength(1, 'Please enter the password.'),
+  ]),
+  name: string('Name must be a string', [
+    minLength(1, 'Please enter the name.'),
+  ]),
+  principalUserId: string('Principal user id must be a string'),
+  email: string('Email must be a string', [
+    email('Please enter a valid email address.'),
+  ]),
 });
 
 export const CreateAccountAdminSchema = object({
-	username: string("Username must be a string", [
-		minLength(1, "Please enter the username."),
-	]),
-	password: string("Password must be a string", [
-		minLength(1, "Please enter the password."),
-	]),
-	name: string("Name must be a string", [
-		minLength(1, "Please enter the name."),
-	]),
-	email: string("Email must be a string", [
-		email("Please enter a valid email address."),
-	]),
+  username: string('Username must be a string', [
+    minLength(1, 'Please enter the username.'),
+  ]),
+  password: string('Password must be a string', [
+    minLength(1, 'Please enter the password.'),
+  ]),
+  name: string('Name must be a string', [
+    minLength(1, 'Please enter the name.'),
+  ]),
+  email: string('Email must be a string', [
+    email('Please enter a valid email address.'),
+  ]),
 });
 
 const PrincipalUserIdsSchema = array(
-	string("Principal user id must be a string"),
-	"The input must be an array of principal user ids.",
-	[minLength(1, "Please input at least one principal user id.")],
+  string('Principal user id must be a string'),
+  'The input must be an array of principal user ids.',
+  [minLength(1, 'Please input at least one principal user id.')]
 );
 export const CreateAccountAdminBulkSchema = object({
-	principalUserIds: PrincipalUserIdsSchema,
+  principalUserIds: PrincipalUserIdsSchema,
 });
 
 export const CreateApproversSchema = object({
-	principalUserIds: PrincipalUserIdsSchema,
+  principalUserIds: PrincipalUserIdsSchema,
 });
 
 export const DeleteApproverSchema = object({
-	userId: string("User id must be a string", [minLength(5)]),
+  userId: string('User id must be a string', [minLength(5)]),
 });
 
 export const UpdateAccountUserSchema = object({
-	name: optional(
-		string("Name must be a string", [minLength(1, "Please enter the name.")]),
-	),
-	username: optional(
-		string("Username must be a string", [
-			minLength(1, "Please enter the username."),
-		]),
-	),
-	principalUserId: optional(
-		string("Principal user id must be a string", [
-			uuid("Please enter a valid principal user id."),
-		]),
-	),
-	id: string("Id must be a string", [minLength(1, "Please enter the id.")]),
+  name: optional(
+    string('Name must be a string', [minLength(1, 'Please enter the name.')])
+  ),
+  username: optional(
+    string('Username must be a string', [
+      minLength(1, 'Please enter the username.'),
+    ])
+  ),
+  principalUserId: optional(string('Principal user id must be a string')),
+  id: string('Id must be a string', [minLength(1, 'Please enter the id.')]),
 });
 
 export type DeleteApproverData = Output<typeof DeleteApproverSchema>;
 export type CreateAccountAdminBulkData = Output<
-	typeof CreateAccountAdminBulkSchema
+  typeof CreateAccountAdminBulkSchema
 >;
 export type CreateApproversData = Output<typeof CreateApproversSchema>;
 export type CreateAccountUserData = Output<typeof CreateAccountUserSchema>;
@@ -115,10 +106,10 @@ export type CreateAccountAdminData = Output<typeof CreateAccountAdminSchema>;
 export type UpdateAccountUserData = Output<typeof UpdateAccountUserSchema>;
 export type DeleteAccountsData = Output<typeof DeleteAccountsSchema>;
 export type UpdateAccountPasswordData = Output<
-	typeof UpdateAccountPasswordSchema
+  typeof UpdateAccountPasswordSchema
 > & {
-	userId: string;
+  userId: string;
 };
 export type ResetAccountUserPasswordData = Output<
-	typeof ResetAccountUserPasswordSchema
+  typeof ResetAccountUserPasswordSchema
 >;
